5bea435d1f7bc54fb0f0f9daa607f31edaee59a7
unknown <Fermin Molinuevo@vpa-fmolinuevo.bellevue.velocitypartners.net>
2015-04-20 15:03:15 -0300

    project

---
+++ TcExample/Controllers/PersonCrudController.cs
@@ -0,0 +1,123 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+using TcExample.Models;
+
+namespace TcExample.Controllers
+{
+    public class PersonCrudController : Controller
+    {
+        private PersonContext db = new PersonContext();
+
+        //
+        // GET: /PersonCrud/
+
+        public ActionResult Index()
+        {
+            return View(db.UserProfiles.ToList());
+        }
+
+        //
+        // GET: /PersonCrud/Details/5
+
+        public ActionResult Details(int id = 0)
+        {
+            UserProfile userprofile = db.UserProfiles.Find(id);
+            if (userprofile == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userprofile);
+        }
+
+        //
+        // GET: /PersonCrud/Create
+
+        public ActionResult Create()
+        {
+            return View();
+        }
+
+        //
+        // POST: /PersonCrud/Create
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create(UserProfile userprofile)
+        {
+            if (ModelState.IsValid)
+            {
+                db.UserProfiles.Add(userprofile);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            return View(userprofile);
+        }
+
+        //
+        // GET: /PersonCrud/Edit/5
+
+        public ActionResult Edit(int id = 0)
+        {
+            UserProfile userprofile = db.UserProfiles.Find(id);
+            if (userprofile == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userprofile);
+        }
+
+        //
+        // POST: /PersonCrud/Edit/5
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit(UserProfile userprofile)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(userprofile).State = EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            return View(userprofile);
+        }
+
+        //
+        // GET: /PersonCrud/Delete/5
+
+        public ActionResult Delete(int id = 0)
+        {
+            UserProfile userprofile = db.UserProfiles.Find(id);
+            if (userprofile == null)
+            {
+                return HttpNotFound();
+            }
+            return View(userprofile);
+        }
+
+        //
+        // POST: /PersonCrud/Delete/5
+
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            UserProfile userprofile = db.UserProfiles.Find(id);
+            db.UserProfiles.Remove(userprofile);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            db.Dispose();
+            base.Dispose(disposing);
+        }
+    }
+}
---
+++ TcExample/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="TcExample.MvcApplication" Language="C#" %>
